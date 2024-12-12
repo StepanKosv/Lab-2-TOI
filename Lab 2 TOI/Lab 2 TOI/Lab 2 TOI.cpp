9f51164bfb74c5f4345d39050ba4f848a5179244
RomanKosv <romkos2006@gmail.com>
2024-12-12 20:07:06 +0500

    programs

--- Lab 2 TOI/Lab 2 TOI/Lab 2 TOI.cpp
+++ Lab 2 TOI/Lab 2 TOI/Lab 2 TOI.cpp
@@ -2,6 +2,9 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <tuple>
+using namespace std;
 
 int AddIter(int x, int y) {
     int s = x;
@@ -16,10 +19,91 @@ int AddReq(int x, int y) {
     }
     return AddReq(x + 1, y - 1);
 }
+int FibReq(int n) {
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    return FibReq(n - 2) + FibReq(n - 1);
+}
+int FibIter(int n) {
+    vector<int> nums { 1,1 };
+    while (n > nums.size()) {
+        int a = nums[nums.size() - 1];
+        int b = nums[nums.size() - 2];
+        int nxt = a + b;
+        nums.push_back(nxt);
+    }
+    return nums[n - 1];
+}
+int mac(int n) {
+    if (n > 100) {
+        return n - 10;
+    }
+    return mac(mac(n + 11));
+}
+int acc(int m, int n) {
+    if (m == 0) {
+        return n + 1;
+    }
+    if (n == 0) {
+        return acc(m - 1, 1);
+    }
+    return acc(m - 1, acc(m, n - 1));
+}
+vector<vector<int>> input_field(int bordercolor, int fieldcolor) {
+    int m, n;
+    cout << "Введите количество строк\n";
+    cin >> m;
+    cout << "Введите количество столбцов\n";
+    cin >> n;
+    cout << "Введите фигуру по строкам, числа - номера цветов, " 
+        << bordercolor << " - цвет границы, " 
+        << fieldcolor << " - цвет закраски"<<'\n';
+    vector<vector<int>> res{};
+    for (int i = 0;i < m;i++) {
+        res.push_back(vector<int>(n,0));
+        for (int j = 0;j < n;j++) {
+            cin >> res[i][j];
+        }
+    }
+    return res;
+}
+tuple<int,int> input_start_point() {
+    cout << "введите координаты стартовой точки закраски в формате x y\n";
+    int x, y;
+    cin >> x;
+    cin >> y;
+    return tuple<int, int>{ x - 1, y - 1 };
+}
+void fill_field(int x, int y, vector<vector<int>>& field, int bordercolor, int fieldcolor, int fillcolor) {
+    if (x<0 || y<0 || x>=field.size() || y>=field[0].size())
+        return;
+    if (field[x][y] == fieldcolor) {
+        field[x][y] = fillcolor;
+        fill_field(x + 1, y, field, bordercolor, fieldcolor, fillcolor);
+        fill_field(x - 1, y, field, bordercolor, fieldcolor, fillcolor);
+        fill_field(x, y + 1, field, bordercolor, fieldcolor, fillcolor);
+        fill_field(x, y - 1, field, bordercolor, fieldcolor, fillcolor);
+    }
+}
+void fill_process() {
+    int fillcolor = 1, bordercolor = 2, fieldcolor = 0;
+    vector < vector<int >> field = input_field(bordercolor, fieldcolor);
+    int x, y;
+    tie(x, y) = input_start_point();
+    fill_field(x, y, field, bordercolor, fieldcolor, fillcolor);
+    for (vector<int> v : field) {
+        for (int i : v) {
+            cout << i << ' ';
+        }
+        cout << '\n';
+    }
+}
 
 int main()
 {
-    std::cout << AddIter(4,2)<<' '<<AddReq(4,2)<<'\n';
+    setlocale(LC_ALL, "");
+    fill_process();
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
